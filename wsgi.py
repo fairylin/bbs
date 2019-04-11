7af260ea9750e25dc7fe2947036330d37fae6d42
fairylin <15574412169@163.com>
2019-04-11 16:33:11 +0800

    修改服务器配置 wsgi gunicorn bbs.conf

--- wsgi.py
+++ wsgi.py
@@ -25,4 +25,26 @@ command=/usr/local/bin/gunicorn wsgi --bind 0.0.0.0:2000 --pid /tmp/todo.pid
 directory=/root/web13
 autostart=true
 
+
+建立一个软连接
+ln -s /var/www/bbs/bbs.conf /etc/supervisor/conf.d/bbs.conf
+
+ln -s /var/www/bbs/bbs.nginx /etc/nginx/sites-enabled/bbs
+
+
+
+➜  ~ cat /etc/supervisor/conf.d/bbs.conf
+
+[program:bbs]
+command=/usr/local/bin/gunicorn wsgi -c gunicorn.config.py
+directory=/var/www/bbs
+autostart=true
+autorestart=true
+
+
+
+
+/usr/local/bin/gunicorn wsgi
+--bind 0.0.0.0:2001
+--pid /tmp/飙泪og.pid
 """
