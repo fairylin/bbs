493b94934230e0b93b7e059d6738459f2a045df9
fairylin <15574412169@163.com>
2020-03-15 17:34:57 +0800

    增加编码标识，避免服务器拉取数据运行时报错

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,4 @@
-#encoding:utf8
+# encoding:utf8
 import json
 import time
 from utils import log
--- models/user.py
+++ models/user.py
@@ -1,3 +1,4 @@
+# encoding:utf8
 from models import Model
 
 
@@ -6,6 +7,7 @@ class User(Model):
     User 是一个保存用户数据的 model
     现在只有两个属性 username 和 password
     """
+
     def __init__(self, form):
         self.id = form.get('id', None)
         self.username = form.get('username', '')
@@ -15,6 +17,7 @@ class User(Model):
         import hashlib
         def sha256(ascii_str):
             return hashlib.sha256(ascii_str.encode('ascii')).hexdigest()
+
         hash1 = sha256(password)
         hash2 = sha256(hash1 + salt)
         return hash2
